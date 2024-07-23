64f790980d82b41fa40b9ba5f0257ee17535105d
Kizzo-liso <131163481+Kizzo-liso@users.noreply.github.com>
2024-07-23 10:08:29 -0300

    fiz um commitas

---
+++ Kia Med - Projeto/app/pages/header.php
@@ -0,0 +1,18 @@
+<header>
+        <button 
+        style="background: none; border: none"
+         id="toggle-dark-mode"> Dark Mode <i class="bi bi-moon-fill"></i></button>
+
+        <div id="titulo">
+            <h1>Kia Med</h1>
+        </div>
+        <nav>
+            <ul>
+                <a href="#"><li>Serviços</li></a>
+                <a href="#"><li>Contatos</li></a>
+                <a href="sobrenos.html"><li>Sobre</li></a>
+                <a href="feedback.html"><li>Feedback</li></a>
+                <a href="aleatorio.html" id="inscreva-se"><li>Já tem uma conta?</li></a>
+            </ul>
+        </nav> 
+    </header>
--- Kia Med - Projeto/app/pages/homo pongo.php
+++ Kia Med - Projeto/app/pages/homo pongo.php
@@ -5,30 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kia Med</title>
     <link rel="stylesheet" href="../public/css/homea.css">
+    <link rel="stylesheet" href="../public/css/mododark.css">
     <link rel="stylesheet" href="css/media.css">
     <script src="../public/js/home.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 <body>
     <!--  header é um cabeçalho usado para deixá o conteúdo fixo no topo. -->
-    <header>
-        <button 
-        style="background: none; border: none"
-         id="toggle-dark-mode"> Dark Mode <i class="bi bi-moon-fill"></i></button>
-
-        <div id="titulo">
-            <h1>Kia Med</h1>
-        </div>
-        <nav>
-            <ul>
-                <a href="#"><li>Serviços</li></a>
-                <a href="#"><li>Contatos</li></a>
-                <a href="sobrenos.html"><li>Sobre</li></a>
-                <a href="feedback.html"><li>Feedback</li></a>
-                <a href="aleatorio.html" id="inscreva-se"><li>Já tem uma conta?</li></a>
-            </ul>
-        </nav> 
-    </header>
+<?php include_once "header.php" ?>
     <!-- main define o conteúdo principal dentro do <body> em seu documento ou aplicação. Entende-se como conteúdo principal aquele relacionado diretamente com o tópico central da página ou com a funcionalidade central da aplicação. -->
     <main>
         <!-- O elemento aside define um bloco de conteúdo que faz referência ao conteúdo principal que o cerca  -->
